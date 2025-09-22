14a81ee1ca86c390b6ec14f7703ffa794d51ca89
chad <chadrogers0010@gmail.com>
2025-09-21 20:01:25 -0700

    Add fn new(); Change return types to anyhow::Result<T>

--- api/src/in_memory_store.rs
+++ api/src/in_memory_store.rs
@@ -2,24 +2,32 @@ use common::LogEntry;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
-#[derive(Clone, Default)]
+#[derive(Clone, Debug, Default)]
 pub struct InMemoryStore {
     logs: Arc<RwLock<Vec<LogEntry>>>,
 }
 
+impl InMemoryStore {
+    pub fn new() -> Self {
+        Self {
+            logs: Arc::new(RwLock::new(Vec::new())),
+        }
+    }
+}
+
 #[axum::async_trait]
 impl crate::LogStore for InMemoryStore {
-    async fn list_logs(&self) -> Vec<LogEntry> {
-        self.logs.read().await.clone()
+    async fn list_logs(&self) -> anyhow::Result<Vec<LogEntry>> {
+        Ok(self.logs.read().await.clone())
     }
 
-    async fn add_log(&self, message: String) -> LogEntry {
+    async fn add_log(&self, message: String) -> anyhow::Result<LogEntry> {
         let entry = LogEntry::new(message);
         self.logs.write().await.push(entry.clone());
-        entry
+        Ok(entry)
     }
 
-    async fn count(&self) -> usize {
-        self.logs.read().await.len()
+    async fn count(&self) -> anyhow::Result<usize> {
+        Ok(self.logs.read().await.len())
     }
 }
